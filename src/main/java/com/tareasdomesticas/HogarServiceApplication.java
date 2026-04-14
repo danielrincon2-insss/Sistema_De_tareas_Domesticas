b07f9f282463313fb73357a84d34fc623d8b58a6
Alejandra2106 <malejandra.otalvaro1@udea.edu.co>
2026-04-14 14:34:51 -0500

    Proyecto base en Spring Boot

---
+++ src/main/java/com/tareasdomesticas/HogarServiceApplication.java
@@ -0,0 +1,14 @@
+package com.tareasdomesticas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogarServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogarServiceApplication.class, args);
+	}
+
+}
+
